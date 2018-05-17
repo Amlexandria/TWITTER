aff316e4dfb6a15ab05f71ef55ab8a0bae60ecbf
Amlexandria <antestumejorsonrisa@gmail.com>
2018-05-17 15:02:55 -0500

    Se agrega la primera versión del ejercicio.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 var tweetButton = document.getElementById("button-tweet");
-var tweet = document.getElementById("text-area").value;
+var main = document.getElementById("main");
 
 //CREANDO EL TWEET QUE DESENCADENA EL EVENTO "TWEET BUTTON"
 
 tweetButton.addEventListener("click", function(){
+
+    var valueOfTheTweet = document.getElementById("text-area").value;
+
+//Creando elementos:
+  //Creando text node del value del textarea
+  var textOfTheTweet = document.createTextNode(valueOfTheTweet);
+  console.log(textOfTheTweet);
+  //Creando el div que contendra el tweet
+  var divTweet = document.createElement("div");
+  divTweet.classList.add("font", "container-tweet-text");
+
+  //Creando la sección que contendrá el tweet y la hora
+  var newTweet = document.createElement("section");
+  newTweet.classList.add("new-tweet-width", "position");
+
+//Agregando Elementos:
+  divTweet.appendChild(textOfTheTweet);
+  newTweet.appendChild(divTweet);
+  main.appendChild(newTweet);
+
+  document.getElementById("text-area").value = "";
+
+  var focus = document.getElementById("text-area");
+  focus.focus();
 
 
 });//cerranndo tweetButton
