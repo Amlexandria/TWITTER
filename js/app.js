1e4ee8b4710c8ac9e534ecb3fd8443dc55e4947b
Amlexandria <antestumejorsonrisa@gmail.com>
2018-05-17 13:25:11 -0500

    Se agrega el maquetado básico de la página.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var tweetButton = document.getElementById("button-tweet");
+var tweet = document.getElementById("text-area").value;
+
+//CREANDO EL TWEET QUE DESENCADENA EL EVENTO "TWEET BUTTON"
+
+tweetButton.addEventListener("click", function(){
+
+
+});//cerranndo tweetButton
